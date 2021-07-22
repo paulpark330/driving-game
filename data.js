34b4f7c108a5334ddf316fc8e390d4a84064962b
Paul Park <paulpark330@hotmail.com>
2021-07-22 13:17:18 -0700

    user is able to start the car

--- data.js
+++ data.js
@@ -1,16 +1,9 @@
 /* exported data */
 
 var data = {
-  direction: 'east'
+  direction: 'east',
+  location: {
+    x: 0,
+    y: 0
+  }
 };
-
-var previousDataModel = localStorage.getItem('car');
-
-if (previousDataModel !== null) {
-  data = JSON.parse(previousDataModel);
-}
-
-window.addEventListener('beforeunload', function (event) {
-  var dataJSON = JSON.stringify(data);
-  localStorage.setItem('car', dataJSON);
-});
